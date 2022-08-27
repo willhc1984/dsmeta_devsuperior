4d77fdb89b5d07a68bd513ac1ab0f88e3832dc86
unknown <willhc1984@gmail.com>
2022-08-27 13:13:05 -0300

    Criação classe de negocio - Sale

---
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -0,0 +1,96 @@
+package com.devsuperior.dsmeta.entities;
+
+import java.io.Serializable;
+import java.time.LocalDate;
+import java.util.Objects;
+
+public class Sale implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private Long id;
+	private String sellerName;
+	private Integer visited;
+	private Integer deals;
+	private Double amount;
+	private LocalDate date;
+	
+	public Sale() {
+	}
+
+	public Sale(Long id, String sellerName, Integer visited, Integer deals, Double amount, LocalDate date) {
+		super();
+		this.id = id;
+		this.sellerName = sellerName;
+		this.visited = visited;
+		this.deals = deals;
+		this.amount = amount;
+		this.date = date;
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getSellerName() {
+		return sellerName;
+	}
+
+	public void setSellerName(String sellerName) {
+		this.sellerName = sellerName;
+	}
+
+	public Integer getVisited() {
+		return visited;
+	}
+
+	public void setVisited(Integer visited) {
+		this.visited = visited;
+	}
+
+	public Integer getDeals() {
+		return deals;
+	}
+
+	public void setDeals(Integer deals) {
+		this.deals = deals;
+	}
+
+	public Double getAmount() {
+		return amount;
+	}
+
+	public void setAmount(Double amount) {
+		this.amount = amount;
+	}
+
+	public LocalDate getDate() {
+		return date;
+	}
+
+	public void setDate(LocalDate date) {
+		this.date = date;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Sale other = (Sale) obj;
+		return Objects.equals(id, other.id);
+	}
+	
+}
