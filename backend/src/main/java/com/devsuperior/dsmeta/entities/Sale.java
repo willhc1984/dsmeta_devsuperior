44dad52915e5fa3a1202c5f1ccabf0cf61e5e54c
unknown <willhc1984@gmail.com>
2022-08-27 15:51:25 -0300

    Sales - repository/service/controller

--- backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
+++ backend/src/main/java/com/devsuperior/dsmeta/entities/Sale.java
@@ -4,10 +4,20 @@ import java.io.Serializable;
 import java.time.LocalDate;
 import java.util.Objects;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_sales")
 public class Sale implements Serializable{
 	
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String sellerName;
 	private Integer visited;
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,9 @@
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 121, 67, 18196.0,'2022-06-16');
