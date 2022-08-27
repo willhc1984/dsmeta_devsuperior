ea86af946846ab3503533878ca9eb95a271515b2
unknown <willhc1984@gmail.com>
2022-08-27 16:32:41 -0300

    Implementação Pageable e consulta por data

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,9 +1,9 @@
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 18196.0,'2022-06-16');
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 121, 67, 18196.0,'2022-06-16');
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 121, 67, 18196.0,'2022-06-16');
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 121, 67, 18196.0,'2022-06-16');
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 18196.0,'1998-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 121, 67, 18196.0,'2002-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 121, 67, 18196.0,'2021-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 121, 67, 18196.0,'2018-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 121, 67, 18196.0,'2022-05-16');
 INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 67, 18196.0,'2022-06-16');
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 121, 67, 18196.0,'2022-06-16');
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 121, 67, 18196.0,'2022-06-16');
-INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 121, 67, 18196.0,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 121, 67, 18196.0,'2022-07-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 121, 67, 18196.0,'2010-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 121, 67, 18196.0,'2011-04-16');
