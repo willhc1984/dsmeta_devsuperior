3ad7d4c3ae891723c0ceb1e9f6d78071900bfca4
will henrique <willhc1984@gmail.com>
2022-08-31 10:44:54 -0300

    Filtro por no data frontend - React

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -7,3 +7,63 @@ INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla',
 INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 341, 561, 784.65,'2022-07-16');
 INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 678, 345, 1254.85,'2010-06-16');
 INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 409, 098, 987.35,'2011-04-16');
+
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 1256.00,'1998-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 456, 89, 896.00,'2002-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 432, 234, 2369.88,'2021-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 678, 678, 1458.36,'2018-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 897, 984, 654.78,'2022-05-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 23, 136.87,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 341, 561, 784.65,'2022-07-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 678, 345, 1254.85,'2010-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 409, 098, 987.35,'2011-04-16');
+
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 1256.00,'1998-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 456, 89, 896.00,'2002-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 432, 234, 2369.88,'2021-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 678, 678, 1458.36,'2018-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 897, 984, 654.78,'2022-05-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 23, 136.87,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 341, 561, 784.65,'2022-07-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 678, 345, 1254.85,'2010-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 409, 098, 987.35,'2011-04-16');
+
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 1256.00,'1998-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 456, 89, 896.00,'2002-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 432, 234, 2369.88,'2021-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 678, 678, 1458.36,'2018-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 897, 984, 654.78,'2022-05-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 23, 136.87,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 341, 561, 784.65,'2022-07-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 678, 345, 1254.85,'2010-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 409, 098, 987.35,'2011-04-16');
+
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 1256.00,'1998-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 456, 89, 896.00,'2002-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 432, 234, 2369.88,'2021-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 678, 678, 1458.36,'2018-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 897, 984, 654.78,'2022-05-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 23, 136.87,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 341, 561, 784.65,'2022-07-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 678, 345, 1254.85,'2010-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 409, 098, 987.35,'2011-04-16');
+
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 1256.00,'1998-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 456, 89, 896.00,'2002-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 432, 234, 2369.88,'2021-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 678, 678, 1458.36,'2018-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 897, 984, 654.78,'2022-05-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 23, 136.87,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 341, 561, 784.65,'2022-07-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 678, 345, 1254.85,'2010-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 409, 098, 987.35,'2011-04-16');
+
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Joselito', 121, 67, 1256.00,'1998-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Maria', 456, 89, 896.00,'2002-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Ana Paula', 432, 234, 2369.88,'2021-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Marcos', 678, 678, 1458.36,'2018-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('João', 897, 984, 654.78,'2022-05-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Karla', 121, 23, 136.87,'2022-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Jose', 341, 561, 784.65,'2022-07-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Gilmar', 678, 345, 1254.85,'2010-06-16');
+INSERT INTO tb_sales(seller_name, visited, deals, amount, date) VALUES ('Pablo', 409, 098, 987.35,'2011-04-16');
--- frontend/src/assets/components/SalesCard/index.tsx
+++ frontend/src/assets/components/SalesCard/index.tsx
@@ -10,7 +10,7 @@ import './style.css'
 
 function SalesCard() {
 
-    const min = new Date(new Date().setDate(new Date().getDate() - 365));
+    const min = new Date(new Date().setDate(new Date().getDate() - 1024));
     const max = new Date();
 
     const [minDate, setMinDate] = useState(min);
@@ -19,11 +19,18 @@ function SalesCard() {
     const [sales, setSales] = useState<Sale[]>();
 
     useEffect(() => {
-        axios.get(`${BASE_URL}/sales/`)
+
+        const dmin = minDate.toISOString().slice(0, 10);
+        const dmax = maxDate.toISOString().slice(0, 10);
+
+        console.log(dmin);
+        console.log(dmax);
+
+        axios.get(`${BASE_URL}/sales?minDate=${dmin}&maxDate=${dmax}`)
             .then(response => {
                 setSales(response.data.content);
             })
-    }, []);
+    }, [minDate, maxDate]);
 
     return (
         <div className="dsmeta-card">
