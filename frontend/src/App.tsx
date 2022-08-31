80275ebf99275ac6a0f7dd9732bc81575053e388
will henrique <willhc1984@gmail.com>
2022-08-31 11:11:23 -0300

    Notificação com ReactToastify

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,13 @@
 import { useState } from 'react'
 import Header from './assets/components/Header'
 import SalesCard from './assets/components/SalesCard'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css';
 
 function App() {
   return (
     <>
+    <ToastContainer />
       <Header />
       <main>
         <section id="sales">
--- frontend/src/assets/components/NotificationButton/index.tsx
+++ frontend/src/assets/components/NotificationButton/index.tsx
@@ -1,11 +1,16 @@
+import { toast } from "react-toastify"
 import icon from "../../img/notification-icon.svg"
 import './style.css'
+
+function handleClick(){
+    toast.info("SMS enviado com sucesso!");
+}
 
 
 function NotificationButton() {
     return (
 
-        <div className="dsmeta-red-btn">
+        <div className="dsmeta-red-btn" onClick={() => handleClick()}>
             <img src={icon} alt="notificar" />
         </div>
 
